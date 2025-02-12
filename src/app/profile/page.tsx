b417f880f7924a95443d37b39978e96026109152
Prabhmeet Singh <98380208+Prabhmeetchef@users.noreply.github.com>
2025-02-12 23:49:58 +0530

    Build fixes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,34 +1,31 @@
 "use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Profile() {
-  const handle = localStorage.getItem("userHandle");
-  const password = localStorage.getItem("userPassword");
+  const [handle, setHandle] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHandle(localStorage.getItem("userHandle"));
+      setPassword(localStorage.getItem("userPassword"));
+    }
+  }, []);
 
-
-
-
-
-
-
-
-
-
-
-
-
-  
   return (
     <main className="flex flex-col items-center min-h-screen p-6 bg-black">
       <header className="w-full mb-12">
-        <Link href={'/dashboard'}><Image
-          src="/whotops_logo.png"
-          alt="Whotops Logo"
-          width={120}
-          height={60}
-        /></Link>
+        <Link href={'/dashboard'}>
+          <Image
+            src="/whotops_logo.png"
+            alt="Whotops Logo"
+            width={120}
+            height={60}
+          />
+        </Link>
       </header>
       <p className="text-white mt-10">
         <span className="text-[#8b8b8b]">Name:</span> {handle}{" "}
